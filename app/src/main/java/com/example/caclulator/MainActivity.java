9286470aa6825b075a66d7fa335a6448a869ec42
MarioGezu <gezaviola10@gmail.com>
2025-10-16 13:33:09 +0200

    Meghívások megírva

--- app/src/main/java/com/example/caclulator/MainActivity.java
+++ app/src/main/java/com/example/caclulator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.caclulator;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText studentDiscounts, travelExpenses, ticketExpenses, numberOfStudents, numberOfGrownUps, houseExpenses;
+    TextView textView, textView2, textView3;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        initialize();
+    }
+    public void initialize(){
+        studentDiscounts = (EditText)findViewById(R.id.studentDiscounts);
+        travelExpenses = (EditText)findViewById(R.id.travelExpenses);
+        ticketExpenses = (EditText)findViewById(R.id.ticketExpenses);
+        numberOfStudents = (EditText)findViewById(R.id.numberOfStudents);
+        numberOfGrownUps = (EditText)findViewById(R.id.numberOfGrownUps);
+        houseExpenses = (EditText)findViewById(R.id.houseExpenses);
+        textView = (TextView)findViewById(R.id.textView);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id.textView3);
     }
 }
+
